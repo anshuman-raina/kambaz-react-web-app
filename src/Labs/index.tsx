ff0dffa5416d806dffcd4e60fe36b95ba1df7cb1
Anshuman Raina <raina.ans@northeastern.edu>
2025-01-20 15:25:12 -0500

    Added changes for A1

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,6 +7,8 @@ import Lab3 from "./Lab3";
 export default function Labs() {
   return (
     <div>
+      <h1>Anshuman Raina</h1>
+      <h2>Section 1</h2>
       <h1>Labs</h1>
       <TOC />
       <Routes>
