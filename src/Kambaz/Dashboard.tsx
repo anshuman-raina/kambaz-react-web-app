c58a459224a06a7f381204110a67402091dc2386
Anshuman Raina <raina.ans@northeastern.edu>
2025-02-05 00:22:48 -0500

    Added changes till 2.4.6

--- src/Kambaz/Dashboard.tsx
+++ src/Kambaz/Dashboard.tsx
@@ -1,98 +1,106 @@
+import { Col, Card, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-export default function Dashboard() {
+
+export default function DashboardCourses() {
   return (
+
     <div id="wd-dashboard">
-      <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
-      <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
-      <div id="wd-dashboard-courses">
-        <div className="wd-dashboard-course">
-          <Link to="/Kambaz/Courses/1234/Home"
-                className="wd-dashboard-course-link" >
-            <img src="/images/reactjs.jpg" width={200} />
-            <div>
-              <h5> CS1234 React JS </h5>
-              <p className="wd-dashboard-course-title">
-                Full Stack software developer  </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course-1"> 
-        <Link to="/Kambaz/Courses/1234/Home"
-                className="wd-dashboard-course-link" >
-            <img src="/images/reactjs.jpg" width={200} />
-            <div>
-              <h5> CS5100 React JS </h5>
-              <p className="wd-dashboard-course-title">
-                Full Stack software developer  </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course-2"> 
-        <Link to="/Kambaz/Courses/1234/Home"
-                className="wd-dashboard-course-link" >
-            <img src="/images/reactjs.jpg" width={200} />
-            <div>
-              <h5> CS5200 React JS </h5>
-              <p className="wd-dashboard-course-title">
-                Full Stack software developer  </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
+      <h1 id="wd-dashboard-title">Dashboard</h1>
+      <hr />
+      <h2 id="wd-dashboard-published">Published Courses (12)</h2>
+      <hr />
+      <div id="wd-dashboard-courses" className="row">
+        <Col className="wd-dashboard-course" style={{ maxWidth: "270px", minWidth: "250px", margin: "30px 0" }}>
+          <Card>
+            <Link to="/Kambaz/Courses/5010/Home" className="wd-dashboard-course-link text-decoration-none text-dark">
+              <Card.Img variant="top" src="/images/reactjs.jpg" width="100%" height={160} alt="Course" />
+              <Card.Body>
+                <Card.Title className="wd-dashboard-course-title">CS5010 PDP</Card.Title>
+                <Card.Text className="wd-dashboard-course-description">Term: Spring 2025 | Year: 2025 | Sec: A</Card.Text>
+                <Button variant="primary">Go</Button>
+              </Card.Body>
+            </Link>
+          </Card>
+        </Col>
 
-        <div className="wd-dashboard-course-2"> 
-        <Link to="/Kambaz/Courses/1234/Home"
-                className="wd-dashboard-course-link" >
-            <img src="/images/reactjs.jpg" width={200} />
-            <div>
-              <h5> CS5610 React JS </h5>
-              <p className="wd-dashboard-course-title">
-                Full Stack software developer  </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
+        <Col className="wd-dashboard-course" style={{ maxWidth: "270px", minWidth: "250px", margin: "30px 0" }}>
+          <Card>
+            <Link to="/Kambaz/Courses/5200/Home" className="wd-dashboard-course-link text-decoration-none text-dark">
+              <Card.Img variant="top" src="/images/reactjs.jpg" width="100%" height={160} alt="Course" />
+              <Card.Body>
+                <Card.Title className="wd-dashboard-course-title">CS5200 DBMS</Card.Title>
+                <Card.Text className="wd-dashboard-course-description">Term: Spring 2025 | Year: 2025 | Sec: A</Card.Text>
+                <Button variant="primary">Go</Button>
+              </Card.Body>
+            </Link>
+          </Card>
+        </Col>
 
-        <div className="wd-dashboard-course-2"> 
-        <Link to="/Kambaz/Courses/1234/Home"
-                className="wd-dashboard-course-link" >
-            <img src="/images/reactjs.jpg" width={200} />
-            <div>
-              <h5> CS5340 React JS </h5>
-              <p className="wd-dashboard-course-title">
-                Full Stack software developer  </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
+        <Col className="wd-dashboard-course" style={{ maxWidth: "270px", minWidth: "250px", margin: "30px 0" }}>
+          <Card>
+            <Link to="/Kambaz/Courses/5800/Home" className="wd-dashboard-course-link text-decoration-none text-dark">
+              <Card.Img variant="top" src="/images/reactjs.jpg" width="100%" height={160} alt="Course" />
+              <Card.Body>
+                <Card.Title className="wd-dashboard-course-title">CS5800 Algorithms</Card.Title>
+                <Card.Text className="wd-dashboard-course-description">Term: Spring 2025 | Year: 2025 | Sec: A</Card.Text>
+                <Button variant="primary">Go</Button>
+              </Card.Body>
+            </Link>
+          </Card>
+        </Col>
 
-        <div className="wd-dashboard-course-2"> 
-        <Link to="/Kambaz/Courses/1234/Home"
-                className="wd-dashboard-course-link" >
-            <img src="/images/reactjs.jpg" width={200} />
-            <div>
-              <h5> CS7565 React JS </h5>
-              <p className="wd-dashboard-course-title">
-                Full Stack software developer  </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
+        <Col className="wd-dashboard-course" style={{ maxWidth: "270px", minWidth: "250px", margin: "30px 0" }}>
+          <Card>
+            <Link to="/Kambaz/Courses/5100/Home" className="wd-dashboard-course-link text-decoration-none text-dark">
+              <Card.Img variant="top" src="/images/reactjs.jpg" width="100%" height={160} alt="Course" />
+              <Card.Body>
+                <Card.Title className="wd-dashboard-course-title">CS5100 FAI</Card.Title>
+                <Card.Text className="wd-dashboard-course-description">Term: Spring 2025 | Year: 2025 | Sec: A</Card.Text>
+                <Button variant="primary">Go</Button>
+              </Card.Body>
+            </Link>
+          </Card>
+        </Col>
 
-        <div className="wd-dashboard-course-2"> 
-        <Link to="/Kambaz/Courses/1234/Home"
-                className="wd-dashboard-course-link" >
-            <img src="/images/reactjs.jpg" width={200} />
-            <div>
-              <h5> CS5100 React JS </h5>
-              <p className="wd-dashboard-course-title">
-                Full Stack software developer  </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
+        <Col className="wd-dashboard-course" style={{ maxWidth: "270px", minWidth: "250px", margin: "30px 0" }}>
+          <Card>
+            <Link to="/Kambaz/Courses/5610/Home" className="wd-dashboard-course-link text-decoration-none text-dark">
+              <Card.Img variant="top" src="/images/reactjs.jpg" width="100%" height={160} alt="Course" />
+              <Card.Body>
+                <Card.Title className="wd-dashboard-course-title">CS5610 Web Development</Card.Title>
+                <Card.Text className="wd-dashboard-course-description">Term: Spring 2025 | Year: 2025 | Sec: A</Card.Text>
+                <Button variant="primary">Go</Button>
+              </Card.Body>
+            </Link>
+          </Card>
+        </Col>
+
+        <Col className="wd-dashboard-course" style={{ maxWidth: "270px", minWidth: "250px", margin: "30px 0" }}>
+          <Card>
+            <Link to="/Kambaz/Courses/3500/Home" className="wd-dashboard-course-link text-decoration-none text-dark">
+              <Card.Img variant="top" src="/images/reactjs.jpg" width="100%" height={160} alt="Course" />
+              <Card.Body>
+                <Card.Title className="wd-dashboard-course-title">CS3500 OOD</Card.Title>
+                <Card.Text className="wd-dashboard-course-description">Term: Spring 2025 | Year: 2025 | Sec: A</Card.Text>
+                <Button variant="primary">Go</Button>
+              </Card.Body>
+            </Link>
+          </Card>
+        </Col>
+
+        <Col className="wd-dashboard-course" style={{ maxWidth: "270px", minWidth: "250px", margin: "30px 0" }}>
+          <Card>
+            <Link to="/Kambaz/Courses/5340/Home" className="wd-dashboard-course-link text-decoration-none text-dark">
+              <Card.Img variant="top" src="/images/reactjs.jpg" width="100%" height={160} alt="Course" />
+              <Card.Body>
+                <Card.Title className="wd-dashboard-course-title">CS5340 Human Computer Interaction</Card.Title>
+                <Card.Text className="wd-dashboard-course-description">Term: Spring 2025 | Year: 2025 | Sec: A</Card.Text>
+                <Button variant="primary">Go</Button>
+              </Card.Body>
+            </Link>
+          </Card>
+        </Col>
       </div>
     </div>
-);}
+  );
+}
--- src/Kambaz/Navigation.tsx
+++ src/Kambaz/Navigation.tsx
@@ -1,14 +1,55 @@
-import { Link } from "react-router-dom";
+import { AiOutlineDashboard } from "react-icons/ai";
+import { IoCalendarOutline } from "react-icons/io5";
+import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
+import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
+import { Link, NavLink } from "react-router-dom";
 export default function KambazNavigation() {
+  const textStyle = {fontSize:'17px'}
   return (
-    <div id="wd-kambaz-navigation">
-      <a href="https://www.northeastern.edu/" id="wd-neu-link" target="_blank">Northeastern</a><br/>
-      <Link to="/Kambaz/Account" id="wd-account-link">Account</Link><br/>
-      <Link to="/Kambaz/Dashboard" id="wd-dashboard-link">Dashboard</Link><br/>
-      <Link to="/Kambaz/Dashboard" id="wd-course-link">Courses</Link><br/>
-      <Link to="/Kambaz/Calendar" id="wd-calendar-link">Calendar</Link><br/>
-      <Link to="/Kambaz/Inbox" id="wd-inbox-link">Inbox</Link><br/>
-      <Link to="/Labs" id="wd-labs-link">Labs</Link><br/>
-    </div>
-);}
+    <div id="wd-kambaz-navigation" style={{ width: 120} }
+      className="list-group rounded-0 position-fixed
+         bottom-0 top-0 d-none d-md-block bg-black z-2">
 
+      <a id="wd-neu-link" target="_blank"
+        href="https://www.northeastern.edu/"
+        className="list-group-item bg-black border-0 text-center">
+        <img src="/images/NEU.png" width="75px" alt="NEU" /></a>
+
+      <NavLink to="/Kambaz/Account" id="wd-account-link"
+        className={({ isActive }) => `list-group-item text-center border-0
+         ${isActive ? "bg-white text-danger" : "bg-black text-white"}`}>
+        <FaRegCircleUser className="fs-1" /><br />
+        Account</NavLink>
+
+      <NavLink to="/Kambaz/Dashboard" id="wd-dashboard-link"
+        className={({ isActive }) => `list-group-item text-center border-0
+        ${isActive ? "bg-white text-danger" : "bg-black text-white"}`} style={textStyle}>
+        <AiOutlineDashboard className="fs-1 text-danger" /><br />
+        Dashboard</NavLink>
+
+      <NavLink to="/Kambaz/Courses" id="wd-course-link"
+        className={({ isActive }) => `list-group-item text-center border-0 
+        ${isActive ? "bg-white text-danger" : "bg-black text-white"}`}>
+        <LiaBookSolid className="fs-1 text-danger" /><br />
+        Courses</NavLink>
+
+      <NavLink to="/Kambaz/Calendar" id="wd-calendar-link"
+        className={({ isActive }) => `list-group-item text-center border-0 
+        ${isActive ? "bg-white text-danger" : "bg-black text-white"}`}>
+        <IoCalendarOutline className="fs-1 text-danger" /><br />
+        Calendar</NavLink>
+
+      <NavLink to="/Kambaz/Inbox" id="wd-inbox-link"
+        className={({ isActive }) => `list-group-item text-center border-0 
+      ${isActive ? "bg-white text-danger" : "bg-black text-white"}`}>
+        <FaInbox className="fs-1 text-danger" /><br />
+        Inbox</NavLink>
+
+      <Link to="/Labs" id="wd-labs-link" className="list-group-item text-white
+                   bg-black text-center border-0">
+        <LiaCogSolid className="fs-1 text-danger" /><br />
+        Labs</Link>
+
+    </div>
+  );
+}
