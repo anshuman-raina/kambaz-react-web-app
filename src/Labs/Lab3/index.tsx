8a2fb1684191c1ef9d2cde8be01591fe88cfdc37
Anshuman Raina <raina.ans@northeastern.edu>
2025-03-05 18:18:59 -0500

    Exercise for Lab done

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -31,14 +31,27 @@ import Add from "./Add";
 import Square from "./Square";
 import Highlight from "./Highlight";
 import PathParameters from "./PathParameters";
+import { useSelector } from "react-redux";
+import { ListGroup } from "react-bootstrap";
 
 export default function Lab3() {
+  const { todos } = useSelector((state: any) => state.todosReducer);
   
+
   console.log('Hello World!');
   return(
     <div id="wd-lab3">
 
       <h3>Lab 3</h3>
+      <ListGroup>
+        {todos.map((todo: any) => (
+          <ListGroup.Item key={todo.id}>
+            {todo.title}
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+      <hr />
+
       <h4>Javascript</h4>
       <VariablesAndConstants/>
       <VariableTypes></VariableTypes>
