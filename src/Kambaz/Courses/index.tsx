6aef16eca53aad31055773a3ed053d9f9155cdad
Anshuman Raina <raina.ans@northeastern.edu>
2025-03-06 18:14:04 -0500

    Changes till 4.4.3 added

--- src/Kambaz/Courses/index.tsx
+++ src/Kambaz/Courses/index.tsx
@@ -1,5 +1,4 @@
 import CourseNavigation from "./Navigation";
-import { courses } from "../Database";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
 import Home from "./Home"
@@ -8,7 +7,7 @@ import PeopleTable from "../People/table";
 import { Navigate, Route, Routes, useParams, useLocation } from "react-router";
 import { FaAlignJustify } from "react-icons/fa";
 
-export default function Courses() {
+export default function Courses({ courses }: { courses: any[]; }) {
   const { cid } = useParams();
   const course = courses.find((course) => course._id === cid);
   const { pathname } = useLocation();
--- src/Kambaz/Dashboard.tsx
+++ src/Kambaz/Dashboard.tsx
@@ -1,11 +1,36 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { v4 as uuidv4 } from 'uuid';
+
 import * as db from "./Database";
-import { Row, Col, Card, Button } from "react-bootstrap";
-export default function Dashboard() {
-  const courses = db.courses;
+import { Row, Col, Card, Button, FormControl } from "react-bootstrap";
+export default function Dashboard(
+  { courses, course, setCourse, addNewCourse,
+    deleteCourse, updateCourse }: {
+    courses: any[]; course: any; setCourse: (course: any) => void;
+    addNewCourse: () => void; deleteCourse: (course: any) => void;
+    updateCourse: () => void; }){
+
   return (
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+      <h5>New Course
+        <button className="btn btn-primary float-end"
+          id="wd-add-new-course-click"
+          onClick={addNewCourse} > Add </button>
+        <button className="btn btn-warning float-end me-2"
+          onClick={updateCourse} id="wd-update-course-click">
+          Update
+        </button>
+      </h5>
+
+      <br />
+      <FormControl value={course.name} className="mb-2" onChange={(e) => setCourse({ ...course, name: e.target.value })} />
+      <FormControl value={course.description} as="textarea" rows={3}
+        onChange={(e) => setCourse({ ...course, description: e.target.value })} />
+
+
+      <hr />
       <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
       <div id="wd-dashboard-courses">
         <Row xs={1} md={5} className="g-4">
@@ -13,7 +38,7 @@ export default function Dashboard() {
             <Col className="wd-dashboard-course" style={{ width: "300px" }}>
               <Card>
                 <Link to={`/Kambaz/Courses/${course._id}/Home`}
-                      className="wd-dashboard-course-link text-decoration-none text-dark" >
+                  className="wd-dashboard-course-link text-decoration-none text-dark" >
                   <Card.Img src="/images/reactjs.jpg" variant="top" width="100%" height={160} />
                   <Card.Body className="card-body">
                     <Card.Title className="wd-dashboard-course-title text-nowrap overflow-hidden">
@@ -21,6 +46,27 @@ export default function Dashboard() {
                     <Card.Text className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
                       {course.description} </Card.Text>
                     <Button variant="primary"> Go </Button>
+
+
+                    <button onClick={(event) => {
+                      event.preventDefault();
+                      deleteCourse(course._id);
+                    }} className="btn btn-danger float-end me-2"
+                      id="wd-delete-course-click">
+                      Delete
+                    </button>
+
+
+                    <button id="wd-edit-course-click"
+                      onClick={(event) => {
+                        event.preventDefault();
+                        setCourse(course);
+                      }}
+                      className="btn btn-warning float-end me-2" >
+                      Edit
+                    </button>
+
+
                   </Card.Body>
                 </Link>
               </Card>
@@ -28,4 +74,5 @@ export default function Dashboard() {
           ))}
         </Row>
       </div>
-    </div>);}
+    </div>);
+}
