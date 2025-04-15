60906053afd3d66522cad5a930b24faf50832ba9
Anshuman Raina <raina.ans@northeastern.edu>
2025-04-15 00:14:55 -0400

    Changes for netlify issues

--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -65,7 +65,7 @@ export default function Kambaz() {
     setCourses((courses) => [...courses, newCourse]);
   };
   const deleteCourse = async (courseId: any) => {
-    const status = await courseClient.deleteCourse(courseId);
+     await courseClient.deleteCourse(courseId);
     setCourses(courses.filter((course) => course._id !== courseId));
   };
   const updateCourse = async () => {
